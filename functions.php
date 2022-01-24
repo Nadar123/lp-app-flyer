5362c63033874f170c6eb34bdcd3bc46d43b6b7a
nadar123 <nadar.rosano@gmail.com>
2022-01-24 19:34:49 +0200

    save function and sidebar

--- functions.php
+++ functions.php
@@ -76,3 +76,84 @@ function phi_theme_support() {
   remove_theme_support( 'widgets-block-editor' );
 }
 add_action( 'after_setup_theme', 'phi_theme_support' );
+
+
+
+// ********* BEGIN HERE *********
+
+function sv_related_posts($args = array()) {
+
+  global $post;
+
+  // default args
+  $args = wp_parse_args($args, array(
+      'post_id' => !empty($post) ? $post->ID : '',
+      'taxonomy' => 'category',
+      'limit' => 6,
+      'post_type' => !empty($post) ? $post->post_type : 'post',
+      'orderby' => 'rand'
+  ));
+// print_r($args);
+// die();
+  // check taxonomy
+  if (!taxonomy_exists($args['taxonomy'])) {
+      return;
+  }
+
+  // post taxonomies
+  $taxonomies = wp_get_post_terms($args['post_id'], $args['taxonomy'], array('fields' => 'ids'));
+
+  if (empty($taxonomies)) {
+      return;
+  }
+
+    $post_types = get_post_types( array('public' => true, '_builtin' => false ), 'objects', 'and' );
+    foreach( $post_types as $post_type_object ){
+        echo '<h2>' . $post_type_object->label . '</h2>';
+    }
+    die();
+
+  // query
+  $related_posts = get_posts(array(
+      'post__not_in' => (array)$args['post_id'],
+      'post_type' => 'any',
+      'tax_query' => array(
+          array(
+              'taxonomy' => $args['taxonomy'],
+              'field' => 'term_id',
+              'terms' => $taxonomies
+          ),
+      ),
+      'posts_per_page' => $args['limit'],
+      'orderby' => $args['orderby'],
+      // 'order' => $args['order']
+  ));
+
+  if (!empty($related_posts)) { ?>
+      <div class="related-posts">
+          <h3 class="widget-title"><?php _e('Related articles', 'textdomain'); ?></h3>
+
+          <ul class="related-posts-list">
+              <?php
+              foreach ($related_posts as $post) {
+                  setup_postdata($post);
+                  ?>
+                  <li>
+                      <a class="title" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                          <?php if (has_post_thumbnail()) { ?>
+                              <div class="thumb">
+                                  <?php echo get_the_post_thumbnail(null, 'medium', array('alt' => the_title_attribute(array('echo' => false)))); ?>
+                              </div>
+                          <?php } ?>
+                          <h4><?php the_title(); ?></h4>
+                      </a>
+                  </li>
+              <?php } ?>
+          </ul>
+          <div class="clearfix"></div>
+      </div>
+      <?php
+  }
+
+  wp_reset_postdata();
+}
--- sidebar.php
+++ sidebar.php
@@ -11,13 +11,7 @@
 
 <?php 
 $all_posts = array(
-        'post_type' => array( 
-            'advanced forex', 
-            'trader interviews',
-            'trading room',
-            'trading psychology',
-            'trade strategy'
-        ),
+        'post_type' => 'any',
         'posts_per_page' => 5,
         'orderby'   => 'menu_order',
         'post_status' => 'publish',
@@ -28,10 +22,11 @@ $query = new WP_Query($all_posts);
 while($query -> have_posts()) : $query -> the_post();
 ?>
 
-<div class="movie">
-
-<h2><?php the_title(); ?> </h2>
-<div><?php the_post_thumbnail('recent_posts'); ?></div>
+<div class="post-type-the5ers">
+    <a href="<?php the_permalink()?>">
+        <h2><?php the_title(); ?> </h2>
+        <div><?php the_post_thumbnail('recent_posts'); ?></div>
+    </a>
 </div>
 
 
