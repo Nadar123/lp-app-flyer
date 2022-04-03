004bdeedc78ed687ab5204c432ac8c97e275eac2
nadar_rossano <nadar@sogo.co.il>
2022-04-03 15:08:58 +0300

    adding content to lp page

--- LandingPage.php
+++ LandingPage.php
@@ -3,13 +3,21 @@
 get_header();
 ?>
     <div class="main-wrapper">
+       
 
         <?php get_template_part('template-parts/landing-page/top-section') ;?>
 
         <?php get_template_part('template-parts/landing-page/key-features') ;?>
 
+        <!-- calling Cards block Plugin -->
+
+        <?php the_content()?>
+
+        <!-- calling Cards block Plugin -->
+
         <?php get_template_part('template-parts/landing-page/contact-form') ;?>
         
+      
     </div>
 
 <?php get_footer(); 
